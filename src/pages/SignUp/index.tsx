e51a43635cf491e9cf90596db8fce3f5d1e8e6fa
DeividMafra <deividmafra@aol.com>
2021-02-12 00:44:42 -0500

    Form Validation + Form styles

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
 
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -10,9 +11,25 @@ import Button from '../../components/Button';
 import { Background, Container, Content } from './styles';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Name is required'),
+                email: Yup.string()
+                    .required('Email is required')
+                    .email('Please enter a valid email'),
+                password: Yup.string().min(
+                    10,
+                    'Please enter at least 10 digits',
+                ),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (error) {
+            console.log(error.inner);
+        }
+    }, []);
 
     return (
         <Container>
